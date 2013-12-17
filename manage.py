487d4bd3a711646ac0666249181dd97cfab22e5e
Anthony Cornehl <accornehl@gmail.com>
2013-12-17 05:22:45 +0000

    Separate the config from manage.py, removing it from global as well

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
-from os.path import dirname, join as pathjoin
+from os.path import dirname, join as pathjoin, exists
 from sys import argv
+import json
 
 from werkzeug import script, create_environ, run_wsgi_app
 
@@ -8,37 +9,58 @@ from lodgeit import local
 from lodgeit.application import make_app
 from lodgeit.database import db
 
-CONFIG = {
-        "db": {
-            "autoflush": True,
-            "uri": "sqlite:///lodgeit.db",
-        },
-# python -c 'import random; print "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])'
-        "secret_key": "no secret key",
-        "disable_captcha": False,
-        "site_title": "Lodge It",
-        "allowed_files": ("jpg", "jpeg", "png", "gif", "bmp", "svg",),
-        "upload_folder": "/tmp",
-}
+DEFAULT_CONFIG = "config_default.json"
+SITE_CONFIG = "config.json"
+
+def load_config(filename):
+    with open(filename) as fd:
+        return json.load(fd)
+
+def apply_config(config, filename):
+    config.update(load_config(filename))
+
+def write_config(filename, data):
+    from random import choice
+    if data.get("secret_key"):
+        data["secret_key"] = "".join([choice(
+            "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)")
+            for i in range(50)])
+    with open(filename, "w") as fd:
+        json.dump(data, fd, indent=4, separators=(',', ': '), sort_keys=True)
+
+def config_generate():
+    workpath = dirname(argv[0])
+    cfg_def = pathjoin(workpath, DEFAULT_CONFIG)
+    cfg_site = pathjoin(workpath, SITE_CONFIG)
+    config = load_config(cfg_def)
+    if not exists(cfg_site):
+        write_config(cfg_site, config)
+        print("Site configuration written to %s" % cfg_site)
+        exit("Please customize the site configuration before restarting the service.")
+    apply_config(config, cfg_site)
+    return config
 
 def run_app(app, path='/'):
-    env = create_environ(path, CONFIG["secret_key"])
+    config = config_generate()
+    env = create_environ(path, config["secret_key"])
     return run_wsgi_app(app, env)
 
 def action_runfcgi():
     from flup.server.fcgi import WSGIServer
-    application = make_app(CONFIG)
-    bindaddr = pathjoin(str(dirname(argv[0])), "werkzeug.sock")
+    workpath = dirname(argv[0])
+    config = config_generate()
+    application = make_app(config)
+    bindaddr = pathjoin(workpath, "werkzeug.sock")
     print "Binding to %s" % (bindaddr,)
     WSGIServer(application, bindAddress=bindaddr).run()
 
 action_runserver = script.make_runserver(
-    lambda: make_app(CONFIG, debug=True),
+    lambda: make_app(config_generate(), debug=True),
     use_reloader=True)
 
 action_shell = script.make_shell(
     lambda: {
-        'app': make_app(CONFIG, False, True),
+        'app': make_app(config_generate(), False, True),
         'local': local,
         'db': db,
         'run_app': run_app,
